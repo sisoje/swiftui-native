dd35d1d178f5a9e07d82bdfeb958c22ccbc9b4e5
Lazar Otasevic <>
2024-09-11 19:17:08 +0400

    fix warnings

--- Sources/ViewHostingApp/PostBodyEvaluation.swift
+++ Sources/ViewHostingApp/PostBodyEvaluation.swift
@@ -1,10 +1,18 @@
-import SwiftUI
+@preconcurrency import SwiftUI
+
+extension Notification.Name {
+    static let bodyEvaluation = Notification.Name("com.ViewHosting.bodyEvaluation")
+}
+
+struct BodyEvaluation: Sendable {
+    let view: any View
+}
 
 public extension View {
     func postBodyEvaluation() {
         assert({
             Self._printChanges()
-            NotificationCenter.default.post(name: .bodyEvaluation, object: self)
+            NotificationCenter.default.post(name: .bodyEvaluation, object: BodyEvaluation(view: self))
         }() == ())
     }
 }
--- Sources/ViewHostingTests/ObserveBodyEvaluation.swift
+++ Sources/ViewHostingTests/ObserveBodyEvaluation.swift
@@ -1,13 +1,6 @@
 import SwiftUI
 @testable import ViewHostingApp
 
-// this silences the "notification is not sendable" warning
-#if swift(>=6.0)
-extension Notification: @unchecked @retroactive Sendable {}
-#else
-extension Notification: @unchecked Sendable {}
-#endif
-
 extension NotificationCenter {
     @MainActor func observeBodyEvaluation<T: View>(timeout: TimeInterval) async throws -> T {
         // normally we get notification immediately but if there is any problem we dont want to wait forever
@@ -16,9 +9,10 @@ extension NotificationCenter {
             // if timeout happens we post nil to unblock the for-await
             post(name: .bodyEvaluation, object: nil)
         }
-        for await notification in notifications(named: .bodyEvaluation) {
+
+        for await bodyEvaluation in notifications(named: .bodyEvaluation).map ({ $0.object as? BodyEvaluation }) {
             timeoutTask.cancel()
-            guard let view = notification.object else {
+            guard let view = bodyEvaluation?.view else {
                 // Make sure you added postBodyEvaluation() in the body of the view
                 throw ViewHostingError.bodyEvaluationTimeout
             }
