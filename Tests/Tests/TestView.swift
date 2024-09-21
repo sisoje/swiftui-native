85bed87bd27dd42132bdc7dfc6b01c8e36a6c90b
Lazar Otasevic <>
2024-09-21 13:41:26 +0400

    simplify

--- Tests/Tests/TestView.swift
+++ Tests/Tests/TestView.swift
@@ -4,13 +4,11 @@ import ViewHosting
 struct TestView: View {
     @Environment(\.onBody) private var onBody
     @State var text = ""
-    
+
     func loadText() async {
-        await MainActor.run {
-            text = "loaded"
-        }
+        text = "loaded"
     }
-    
+
     var body: some View {
         let _ = onBody(self)
     }
